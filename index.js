2a7afa1a3a33a639013be9c06704d014abe9d3b0
Xpl0itU <samuelcarballogarcia8@gmail.com>
2023-03-02 16:46:00 +0100

    Add discord webhook

--- index.js
+++ index.js
@@ -1,98 +1,151 @@
 const fs = require("fs");
-const { ImgurClient } = require('imgur');
+const { ImgurClient } = require("imgur");
 const instagram_upload = require("./instagram_upload");
-const { executablePath } = require('puppeteer');
-const puppeteer = require('puppeteer-extra');
-const StealthPlugin = require('puppeteer-extra-plugin-stealth');
-const util = require('util');
+const { executablePath } = require("puppeteer");
+const puppeteer = require("puppeteer-extra");
+const StealthPlugin = require("puppeteer-extra-plugin-stealth");
+const util = require("util");
 const sleep = util.promisify(setTimeout);
+const webhook = require("webhook-discord");
 
 async function countdown(seconds) {
-    while (seconds) {
-        const mins = Math.floor(seconds / 60);
-        const secs = seconds % 60;
-        const timer = `${mins.toString().padStart(2, '0')}:${secs.toString().padStart(2, '0')}`;
-        process.stdout.write(`\r${timer}`);
-        await sleep(1000);
-        seconds -= 1;
-    }
-    console.log('');
+  while (seconds) {
+    const mins = Math.floor(seconds / 60);
+    const secs = seconds % 60;
+    const timer = `${mins.toString().padStart(2, "0")}:${secs
+      .toString()
+      .padStart(2, "0")}`;
+    process.stdout.write(`\r${timer}`);
+    await sleep(1000);
+    seconds -= 1;
+  }
+  console.log("");
 }
 
 async function scrape() {
-    puppeteer.use(StealthPlugin());
-    const browser = await puppeteer.launch({headless: true, executablePath: executablePath()});
-    const page = (await browser.pages())[0];
-    await page.setViewport({
-        width: 1920,
-        height: 1280,
-        deviceScaleFactor: 1,
-    });
-    await page.setExtraHTTPHeaders({ 
-		'user-agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36', 
-		'upgrade-insecure-requests': '1', 
-		'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8', 
-		'accept-encoding': 'gzip, deflate, br', 
-		'accept-language': 'en-US,en;q=0.9,en;q=0.8' 
-	});
-    await page.goto('https://opensea.io/assets');
+  puppeteer.use(StealthPlugin());
+  const browser = await puppeteer.launch({
+    headless: true,
+    executablePath: executablePath(),
+  });
+  const page = (await browser.pages())[0];
+  await page.setViewport({
+    width: 1920,
+    height: 1280,
+    deviceScaleFactor: 1,
+  });
+  await page.setExtraHTTPHeaders({
+    "user-agent":
+      "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36",
+    "upgrade-insecure-requests": "1",
+    accept:
+      "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8",
+    "accept-encoding": "gzip, deflate, br",
+    "accept-language": "en-US,en;q=0.9,en;q=0.8",
+  });
+  await page.goto("https://opensea.io/assets");
 
-    const element = await page.waitForSelector("main#main>div>div>div>div>div:nth-of-type(3)>div:nth-of-type(3)>div:nth-of-type(3)>div>div:nth-of-type(3)>article>a>div>div>div>div>div>span>img");
-    const imageSrc = await element.getProperty("src");
-    const imageUrl = imageSrc.toString().substring(9).replace("?auto=format&w=3840", "");
+  const element = await page.waitForSelector(
+    "main#main>div>div>div>div>div:nth-of-type(3)>div:nth-of-type(3)>div:nth-of-type(3)>div>div:nth-of-type(3)>article>a>div>div>div>div>div>span>img"
+  );
+  const imageSrc = await element.getProperty("src");
+  const imageUrl = imageSrc
+    .toString()
+    .substring(9)
+    .replace("?auto=format&w=3840", "");
 
-    await page.goto(imageUrl);
-    const imageSelector = await page.waitForSelector("img");
-    await imageSelector.screenshot({path: "to_upload.jpeg", type: "jpeg", quality: 100});
+  await page.goto(imageUrl);
+  const imageSelector = await page.waitForSelector("img");
+  await imageSelector.screenshot({
+    path: "to_upload.jpeg",
+    type: "jpeg",
+    quality: 100,
+  });
 
-    await imageSelector.dispose();
-    await element.dispose();
-    await browser.close();
+  await imageSelector.dispose();
+  await element.dispose();
+  await browser.close();
 }
 
 function onSuccess() {
-    console.log("Successfully uploaded image");
+  const Hook = new webhook.Webhook(process.env.DISCORD_WEBHOOK_URL);
+  var currentdate = new Date();
+  var datetime =
+    currentdate.getDate() +
+    "/" +
+    (currentdate.getMonth() + 1) +
+    "/" +
+    currentdate.getFullYear() +
+    " @ " +
+    currentdate.getHours() +
+    ":" +
+    currentdate.getMinutes() +
+    ":" +
+    currentdate.getSeconds();
+  Hook.success("Success!", "Image successfully uploaded at "`${datetime}`);
 }
 
 function onError(error) {
-    console.log("Error while uploading image");
-    console.log(error);
+  const Hook = new webhook.Webhook(process.env.DISCORD_WEBHOOK_URL);
+  var currentdate = new Date();
+  var datetime =
+    currentdate.getDate() +
+    "/" +
+    (currentdate.getMonth() + 1) +
+    "/" +
+    currentdate.getFullYear() +
+    " @ " +
+    currentdate.getHours() +
+    ":" +
+    currentdate.getMinutes() +
+    ":" +
+    currentdate.getSeconds();
+  Hook.err("Error!", "Error uploading image at "`${datetime}`);
 }
 
 async function scrapeAndUpload() {
-    require('dotenv').config();
-    await scrape();
-    const client = new ImgurClient({ clientId: process.env.IMGUR_CLIENT_ID });
-    const response = await client.upload({
-        image: fs.createReadStream('./to_upload.jpeg'),
-        type: 'stream',
-    });
-    const imgurImgUrl = response.data.link;
-    const dots = `•\n•\n•\n•\n•`;
-    const captions = [
-        "#nfts #nft #nftart #nftcommunity #nftcollector #nftartist #crypto #digitalart #cryptoart #art #ethereum #opensea #nftcollectors #blockchain #nftdrop #cryptocurrency #nftcollectibles #bitcoin #openseanft #nftcollection #cryptoartist #nftartists #eth #nftartgallery #artist #metaverse #nftartwork #artwork #d #artoftheday",
-        "#nftgallery #contemporaryart #nftnews #nftgiveaway #raredigitalart #rarible #btc #cryptonews #nftsstories #digitalartist #digitalcollectibles #defi #modernart #nftmarketplace #design #animation #polygon #artgallery #abstractart #dart #cryptotrading #nonfungibletokens #nftdrops #pixelart #nftmagazine #digitalillustration #meta #nfthesearch #cryptopunks #investing",
-        "#nft #nftart #art #nfts #crypto #nftartist #digitalart #nftcommunity #nftcollector #cryptoart #ethereum #cryptocurrency #opensea #bitcoin #blockchain #d #artist #eth #metaverse #nftcollectors #artwork #nftdrop #nftcollectibles #btc #openseanft #nftcollection #cryptoartist #contemporaryart #nftartists #defi",
-        "#nftartgallery #artoftheday #design #cryptonews #illustration #binance #dart #abstractart #nftartwork #digitalartist #blender #modernart #rarible #abstract #cryptotrading #artgallery #painting #artistsoninstagram #digital #love #dogecoin #animation #drawing #pixelart #nftgallery #polygon #graphicdesign #photography #nftnews #artcollector",
-        "#abstractart #crypto #nfts #abstract #blockchain #metaverse #artistsofinstagram #cryptoartist #surrealism #abstraction #cryptocurrencies #surrealart #aiart #abstractexpressionism #generativeart #digitalcollage #cryptoworld #cryptoinvestor #altcoin #cardano #abstractartwork #cyber #cripto #raredigitalart #cryptocurrencynews #cryptomarket #blockchains #blockchainnews #cryptoinvesting #newmediaart",
-        "#nft #nftart #art #digitalart #nfts #nftartist #crypto #cryptoart #nftcommunity #nftcollector #ethereum #bitcoin #blockchain #cryptocurrency #d #opensea #artist #artwork #eth #nftcollectors #cryptoartist #contemporaryart #nftdrop #nftcollectibles #btc #openseanft #defi #design #artoftheday",
-        "#nftartists #dart #metaverse #nftcollection #blender #cryptonews #rarible #binance #abstractart #abstract #artgallery #modernart #painting #nftartgallery #cryptotrading #animation #pixelart #digitalartist #digital #drawing #dogecoin #artistsoninstagram #raredigitalart #artcollector #nftartwork #music #photography #love #cardano",
-        "#nfts #crypto #blockchain #btc #digitalartist #ethereum #collectibles #pixelart #blender #eth #cryptocurrencies #cryptotrading #cryptonews #binance",
-        "#nft #nftart #art #nfts #crypto #nftartist #digitalart #nftcommunity #nftcollector #cryptoart #ethereum #cryptocurrency #opensea #bitcoin #blockchain #d #artist #eth #metaverse #nftcollectors #artwork #nftdrop #nftcollectibles #btc #openseanft #nftcollection #cryptoartist #contemporaryart #nftartists #defi",
-        "#nftartgallery #artoftheday #design #cryptonews #illustration #binance #dart #abstractart #nftartwork #digitalartist #blender #modernart #rarible #abstract #cryptotrading #artgallery #painting #artistsoninstagram #digital #love #dogecoin #animation #drawing #pixelart #nftgallery #polygon #graphicdesign #photography #nftnews #artcollector",
-    ];
-    const captionEncoded = `For more follow @recent.nft\n${dots}\n${captions[Math.floor(Math.random() * captions.length)]}`;
+  require("dotenv").config();
+  await scrape();
+  const client = new ImgurClient({ clientId: process.env.IMGUR_CLIENT_ID });
+  const response = await client.upload({
+    image: fs.createReadStream("./to_upload.jpeg"),
+    type: "stream",
+  });
+  const imgurImgUrl = response.data.link;
+  const dots = `•\n•\n•\n•\n•`;
+  const captions = [
+    "#nfts #nft #nftart #nftcommunity #nftcollector #nftartist #crypto #digitalart #cryptoart #art #ethereum #opensea #nftcollectors #blockchain #nftdrop #cryptocurrency #nftcollectibles #bitcoin #openseanft #nftcollection #cryptoartist #nftartists #eth #nftartgallery #artist #metaverse #nftartwork #artwork #d #artoftheday",
+    "#nftgallery #contemporaryart #nftnews #nftgiveaway #raredigitalart #rarible #btc #cryptonews #nftsstories #digitalartist #digitalcollectibles #defi #modernart #nftmarketplace #design #animation #polygon #artgallery #abstractart #dart #cryptotrading #nonfungibletokens #nftdrops #pixelart #nftmagazine #digitalillustration #meta #nfthesearch #cryptopunks #investing",
+    "#nft #nftart #art #nfts #crypto #nftartist #digitalart #nftcommunity #nftcollector #cryptoart #ethereum #cryptocurrency #opensea #bitcoin #blockchain #d #artist #eth #metaverse #nftcollectors #artwork #nftdrop #nftcollectibles #btc #openseanft #nftcollection #cryptoartist #contemporaryart #nftartists #defi",
+    "#nftartgallery #artoftheday #design #cryptonews #illustration #binance #dart #abstractart #nftartwork #digitalartist #blender #modernart #rarible #abstract #cryptotrading #artgallery #painting #artistsoninstagram #digital #love #dogecoin #animation #drawing #pixelart #nftgallery #polygon #graphicdesign #photography #nftnews #artcollector",
+    "#abstractart #crypto #nfts #abstract #blockchain #metaverse #artistsofinstagram #cryptoartist #surrealism #abstraction #cryptocurrencies #surrealart #aiart #abstractexpressionism #generativeart #digitalcollage #cryptoworld #cryptoinvestor #altcoin #cardano #abstractartwork #cyber #cripto #raredigitalart #cryptocurrencynews #cryptomarket #blockchains #blockchainnews #cryptoinvesting #newmediaart",
+    "#nft #nftart #art #digitalart #nfts #nftartist #crypto #cryptoart #nftcommunity #nftcollector #ethereum #bitcoin #blockchain #cryptocurrency #d #opensea #artist #artwork #eth #nftcollectors #cryptoartist #contemporaryart #nftdrop #nftcollectibles #btc #openseanft #defi #design #artoftheday",
+    "#nftartists #dart #metaverse #nftcollection #blender #cryptonews #rarible #binance #abstractart #abstract #artgallery #modernart #painting #nftartgallery #cryptotrading #animation #pixelart #digitalartist #digital #drawing #dogecoin #artistsoninstagram #raredigitalart #artcollector #nftartwork #music #photography #love #cardano",
+    "#nfts #crypto #blockchain #btc #digitalartist #ethereum #collectibles #pixelart #blender #eth #cryptocurrencies #cryptotrading #cryptonews #binance",
+    "#nft #nftart #art #nfts #crypto #nftartist #digitalart #nftcommunity #nftcollector #cryptoart #ethereum #cryptocurrency #opensea #bitcoin #blockchain #d #artist #eth #metaverse #nftcollectors #artwork #nftdrop #nftcollectibles #btc #openseanft #nftcollection #cryptoartist #contemporaryart #nftartists #defi",
+    "#nftartgallery #artoftheday #design #cryptonews #illustration #binance #dart #abstractart #nftartwork #digitalartist #blender #modernart #rarible #abstract #cryptotrading #artgallery #painting #artistsoninstagram #digital #love #dogecoin #animation #drawing #pixelart #nftgallery #polygon #graphicdesign #photography #nftnews #artcollector",
+  ];
+  const captionEncoded = `For more follow @recent.nft\n${dots}\n${
+    captions[Math.floor(Math.random() * captions.length)]
+  }`;
 
-    instagram_upload.uploadtoInstagram(process.env.INSTAGRAM_TOKEN, process.env.INSTAGRAM_USER_ID, imgurImgUrl, captionEncoded, onSuccess, onError);
+  instagram_upload.uploadtoInstagram(
+    process.env.INSTAGRAM_TOKEN,
+    process.env.INSTAGRAM_USER_ID,
+    imgurImgUrl,
+    captionEncoded,
+    onSuccess,
+    onError
+  );
 }
 (async () => {
-    while(true) {
-        try {
-            await scrapeAndUpload();
-        } catch {
-            console.error("Error uploading image")
-        } finally {
-            await countdown(3600);
-        }
+  while (true) {
+    try {
+      await scrapeAndUpload();
+    } catch {
+      console.error("Error uploading image");
+    } finally {
+      await countdown(3600);
     }
+  }
 })();
--- instagram_upload.js
+++ instagram_upload.js
@@ -1,49 +1,69 @@
-const { PostPagePhotoMediaRequest, PostPublishMediaRequest } = require("instagram-graph-api");
+const {
+  PostPagePhotoMediaRequest,
+  PostPublishMediaRequest,
+} = require("instagram-graph-api");
 
 const waitUntil = (condition) => {
-    return new Promise((resolve) => {
-        let interval = setInterval(() => {
-            if (!condition())
-                return
+  return new Promise((resolve) => {
+    let interval = setInterval(() => {
+      if (!condition()) return;
 
-            clearInterval(interval)
-            resolve()
-        }, 100)
-    })
-}
+      clearInterval(interval);
+      resolve();
+    }, 100);
+  });
+};
 
 module.exports = {
-    uploadtoInstagram: function(token, pageID, imageURL, imageCaption, callback, errorCallback) {
-        let isLoading = true;
-        let errorHappened = false;
-        let errorString = "Not init";
-        const request = new PostPagePhotoMediaRequest(
+  uploadtoInstagram: function (
+    token,
+    pageID,
+    imageURL,
+    imageCaption,
+    callback,
+    errorCallback
+  ) {
+    let isLoading = true;
+    let errorHappened = false;
+    let errorString = "Not init";
+    const request = new PostPagePhotoMediaRequest(
+      token,
+      pageID,
+      imageURL,
+      imageCaption
+    );
+    request
+      .execute()
+      .then((response) => {
+        errorString = response;
+        if (response.data.id) {
+          const igRequest = new PostPublishMediaRequest(
             token,
             pageID,
-            imageURL,
-            imageCaption
-        );
-        request.execute().then((response) => {
-            errorString = response;
-            if(response.data.id) {
-                const igRequest = new PostPublishMediaRequest(
-                    token,
-                    pageID,
-                    response.data.id
-                );
-        
-                igRequest.execute().then((igResponse) => {
-                    isLoading = false;
-                }).catch(() => { isLoading = false; errorHappened = true; });
-            } else {
-                errorHappened = true;
-                isLoading = false;
-            }
-        }).catch((err) => {
-            isLoading = false;
-            errorHappened = true;
-            errorString = err;
-        });
-        waitUntil(() => isLoading === false).then(() => { errorHappened ? errorCallback(errorString) : callback() });
-    }
- }
+            response.data.id
+          );
+
+          igRequest
+            .execute()
+            .then((igResponse) => {
+              isLoading = false;
+            })
+            .catch(() => {
+              isLoading = false;
+              errorHappened = true;
+            });
+        } else {
+          errorHappened = true;
+          isLoading = false;
+        }
+      })
+      .catch((err) => {
+        isLoading = false;
+        errorHappened = true;
+        errorString = err;
+      });
+    waitUntil(() => isLoading === false).then(() => {
+      errorHappened ? errorCallback(errorString) : callback();
+    });
+  },
+};
